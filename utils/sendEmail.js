7c676626b5c70e3d4bdf202090d6aed14bf62745
tnyxapp <tnyxapp@gmail.com>
2026-03-29 15:22:07 +0530

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,14 +1,15 @@
 const nodemailer = require("nodemailer");
+const dns = require("dns");
+
+// 👇 IMPORTANT FIX (force IPv4)
+dns.setDefaultResultOrder("ipv4first");
 
 const sendEmail = async (to, otp) => {
-  if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
-    throw new Error("Missing EMAIL credentials");
-  }
-
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
+    family: 4, // 👈 force IPv4
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
