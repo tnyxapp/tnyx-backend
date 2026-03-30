8ffd1ce28b00776ba0a1fd500940418b943a2e55
tnyxapp <tnyxapp@gmail.com>
2026-03-30 12:49:04 +0530

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -5,15 +5,18 @@ const sendEmail = async (to, otp) => {
     throw new Error("Email credentials not configured");
   }
 
-  const transporter = nodemailer.createTransport({
-    host: "smtp-relay.brevo.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.BREVO_USER,
-      pass: process.env.BREVO_PASS
-    }
-  });
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 2525, // 👈 change this
+  secure: false,
+  auth: {
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000
+});
 
   try {
     await transporter.sendMail({
