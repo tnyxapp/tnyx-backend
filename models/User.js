e0bbd28fc023b48486bf6947f5f87baf991703ea
tnyxapp <tnyxapp@gmail.com>
2026-03-30 19:51:54 +0530

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    firebaseUid: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    goals: { type: String },
+    gender: { type: String },
+    dob: { type: String },
+    height: { type: String },
+    weight: { type: String },
+    activityLevel: { type: String },
+    mobile: { type: String },
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
