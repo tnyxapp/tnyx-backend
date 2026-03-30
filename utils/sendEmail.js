bb5f01b91f04a9d57a597282d3d9ddd30478f9a7
tnyxapp <tnyxapp@gmail.com>
2026-03-30 11:22:22 +0530

    Update sendEmail.js
    email issue

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -9,16 +9,19 @@ const sendEmail = async (to, otp) => {
     throw new Error("Email credentials not configured");
   }
 
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    family: 4,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
-  });
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  family: 4,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000
+});
 
   // ✅ optional but important check
   await transporter.verify();
