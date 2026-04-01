59f5b3aa6fd093e03327e37a7a7e6283754bd7c7
tnyxapp <tnyxapp@gmail.com>
2026-04-01 07:36:45 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const rateLimit = require("express-rate-limit");
 
-const { signup, googleSync, deleteAccount } = require("../controllers/signupController");
+const { signup, googleSync, deleteAccount, recoverAccount } = require("../controllers/signupController");
 const { sendOtp, verifyOtp } = require("../controllers/otpController");
 const { resetPassword } = require("../controllers/passwordController");
 const { checkUser } = require("../controllers/checkUser");
