40050eec65dcabf54ae374ae1ab5c85fc056e579
tnyxapp <tnyxapp@gmail.com>
2026-04-26 00:48:04 +0530

    Create profileRoutes.js

---
+++ routes/profileRoutes.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+
+const authMiddleware =
+  require("../middlewares/authMiddleware");
+
+const profileController =
+  require("../controllers/profileController");
+
+router.get(
+  "/",
+  authMiddleware,
+  profileController.getProfile
+);
+
+router.patch(
+  "/",
+  authMiddleware,
+  profileController.updateProfile
+);
+
+router.patch(
+  "/image",
+  authMiddleware,
+  profileController.uploadProfileImage
+);
+
+module.exports = router;
