e663af15a00feef1abd09b40d7b2d21ba01177d5
tnyxapp <tnyxapp@gmail.com>
2026-04-26 00:48:29 +0530

    Create healthRoutes.js

---
+++ routes/healthRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+const authMiddleware =
+  require("../middlewares/authMiddleware");
+
+const healthController =
+  require("../controllers/healthController");
+
+router.get(
+  "/",
+  authMiddleware,
+  healthController.getHealthProfile
+);
+
+router.patch(
+  "/",
+  authMiddleware,
+  healthController.updateHealthProfile
+);
+
+module.exports = router;
