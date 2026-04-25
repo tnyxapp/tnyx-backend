a821d12febc899d0af678014ae67426193003109
tnyxapp <tnyxapp@gmail.com>
2026-04-26 00:48:50 +0530

    Create workoutRoutes.js

---
+++ routes/workoutRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const authMiddleware =
+  require("../middlewares/authMiddleware");
+
+const workoutController =
+  require("../controllers/workoutController");
+
+router.get("/", authMiddleware, workoutController.getWorkoutProfile);
+router.patch("/", authMiddleware, workoutController.updateWorkoutProfile);
+
+module.exports = router;
