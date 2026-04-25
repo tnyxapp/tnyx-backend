3c8a3a9f01e417cc8c24f0d107fe625b51ba3af6
tnyxapp <tnyxapp@gmail.com>
2026-04-26 00:55:34 +0530

    Create targetService.js

---
+++ services/targetService.js
@@ -0,0 +1,30 @@
+// services/targetService.js
+
+const express = require("express");
+const router = express.Router();
+
+const authMiddleware =
+  require("../middlewares/authMiddleware");
+
+const targetController =
+  require("../controllers/targetController");
+
+router.get(
+  "/",
+  authMiddleware,
+  targetController.getTargets
+);
+
+router.patch(
+  "/",
+  authMiddleware,
+  targetController.updateTargets
+);
+
+router.post(
+  "/recalculate",
+  authMiddleware,
+  targetController.recalculateTargets
+);
+
+module.exports = router;
